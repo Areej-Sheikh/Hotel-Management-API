6f75d55f0e3ad1ab8c773d0bbec23fae4b314345
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2024-12-16 16:40:54 +0530

    Property Routes Completed

--- src/controllers/property.controller.js
+++ src/controllers/property.controller.js
@@ -4,63 +4,103 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const propertyModel = require("../models/property.model");
 
+// { "title": "", "description": "", "location": "", "price": "", "amenities": "", "images": "" }
 module.exports.createProperty = async (req, res, next) => {
-    const { title, description, location, price, amenities, images } = req.body;
-    try {
-      if (
-        !title ||
-        !description ||
-        !location ||
-        !price ||
-        !amenities ||
-        !images
-      ) {
-        next(new CustomError("All fields are required", 400));
-      }
-      const newProperty = new propertyModel({
-        title,
-        description,
-        location,
-        price,
-        amenities,
-        images,
-        host: req.user._id,
-      });
-      res
-        .status(201)
-        .json({ message: "Property created successfully", newProperty });
-    } catch (error) {
-      next(new CustomError("Error creating property", 500));
+  const { title, description, location, price, amenities, images } = req.body;
+  try {
+    if (!title ||!description ||!location ||!price ||!amenities ||!images) {
+      next(new CustomError("All fields are required", 400));
     }
-  };
-  module.exports.updateProperty = async (req, res, next) => {
+    const newProperty = new propertyModel({title,description,location,price,amenities,images,
+      host: req.user._id,
+    });
+    await newProperty.save();
+    res
+      .status(201)
+      .json({ message: "Property created successfully", newProperty });
+  } catch (error) {
+    next(new CustomError("Error creating property", 500));
+  }
+};
+module.exports.updateProperty = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    if (!id) {
+      next(new CustomError("Property ID is required", 400));
+    }
+    const updateProperty = await propertyModel.findOneAndUpdate(
+      { _id: id },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updateProperty) {
+      return next(new CustomError("Property not found", 404));
+    }
+    res
+      .status(200)
+      .json({ message: "Property updated successfully", updateProperty });
+  } catch (error) {
+    next(new CustomError("Error updating property", 500));
+  }
+};
+module.exports.deleteProperty = async (req, res, next) => {
+  try {
     const { id } = req.params;
-    try {
-      if (!id) {
-        next(new CustomError("Property ID is required", 400));
-      }
-      const updateProperty = await propertyModel.findOneAndUpdate(
-        { _id: id },
-        req.body,
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-      if (!updateProperty) {
-        return next(new CustomError("Property not found", 404));
-      }
-      res
-        .status(200)
-        .json({ message: "Property updated successfully", updateProperty });
-    } catch (error) {
-      next(new CustomError("Error updating property", 500));
+    if (!id) {
+      next(new CustomError("Property ID is required", 400));
     }
-  };
-module.exports.updateProperty = async (req, res, next) =>{
-    try {
-        
-    } catch (error) {
-        
+    const deletedProperty = await propertyModel.findByIdAndDelete(id);
+    if (!deletedProperty) {
+      return next(new CustomError("Property not found", 404));
     }
-}
+    res
+      .status(200)
+      .json({ message: "Property deleted successfully", deletedProperty });
+  } catch (error) {
+    next(new CustomError("Error deleting property", 500));
+  }
+};
+module.exports.viewProperty = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return new CustomError("Property ID is required", 400);
+    }
+    const property = await propertyModel
+      .findById(id)
+      .populate("host", "username email");
+    if (!property) {
+      return next(new CustomError("Property not found", 404));
+    }
+    res.json({ property });
+  } catch (error) {
+    next(new CustomError("Error fetching property details", 500));
+  }
+};
+
+module.exports.searchMyProperties = async (req, res, next) => {
+  try {
+    const properties = await propertyModel.find({ host: req.user._id });
+    res.json({ properties });
+  } catch (error) {
+    next(new CustomError("Error fetching user properties", 500));
+  }
+};
+
+module.exports.searchProperties = async (req, res, next) => {
+  try {
+    const { location, minPrice, maxPrice } = req.query;
+    const query = {
+      ...(location && { location: { $regex: location, $options: "i" } }),
+      ...(minPrice && { minPrice: { $gte: minPrice } }),
+      ...(maxPrice && { maxPrice: { $lte: maxPrice } }),
+    };
+    const properties = await propertyModel.find(query);
+    res.status(200).json({ properties });
+  } catch (error) {
+    next(new CustomError("Error searching properties", 500));
+  }
+};
--- src/routes/property.route.js
+++ src/routes/property.route.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const router = express()
 const {authenticateUser} = require('../middlewares/auth.middleware');
-const {createProperty,updateProperty} = require('../controllers/property.controller')
-
+const {createProperty,updateProperty,deleteProperty,viewProperty,searchMyProperties,searchProperties} = require('../controllers/property.controller')
 
 router.post('/', authenticateUser, createProperty)
+router.get('/me', authenticateUser, searchMyProperties)
+router.get('/search', searchProperties)
 router.put('/:id', authenticateUser, updateProperty)
+router.delete('/:id', authenticateUser, deleteProperty)
+router.get('/:id',viewProperty)
 
-// {
-//     "title":"New Property",
-//      "description":"A Nice Place",
-//       "location": "Bhopal", 
-//       "price": "2000", 
-//       "images":["https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
-//        "amenities": ["Wifi", "Drinking Water", "AC", "Fan"] 
-//     }
-
-module.exports = router;
+module.exports = router; 
