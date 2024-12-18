cbdbd2a682fed16cb8d0f9b5a1d069b55ec822da
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2024-12-18 18:30:26 +0530

    Testing Review Controllers

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -5,29 +5,26 @@ const propertyModel = require("../models/property.model");
 module.exports.addReview = async (req, res, next) => {
   try {
     const { propertyId, rating, comment } = req.body;
-    const property = propertyModel.findById(propertyId);
-    if (!property) {
-      return next(new CustomError("Property not found", 404));
-    }
+    const property = await propertyModel.findById(propertyId);
+    if (!property) return next(new CustomError("Property not found", 404));
     const existingReview = await reviewModel.findOne({
       user: req.user._id,
       property: propertyId,
     });
-    if (existingReview) {
+    if (existingReview)
       return res
         .status(400)
-        .json({ message: "You Have Already Reviewed This Property" });
-    }
+        .json({ error: "You have already reviewed this property" });
     const newReview = new reviewModel({
       property: propertyId,
+      user: req.user._id,
       rating,
       comment,
-      userId: req.user._id,
     });
-    await newReview.save();
-    res.status(201).json({ message: "Review added successfully" });
+    const savedReview = await newReview.save();
+    res.status(201).json(savedReview);
   } catch (error) {
-    next(new CustomError("Could not add review", 500));
+    next(new CustomError("Error adding review", 500));
   }
 };
 module.exports.updateReview = async (req, res, next) => {
@@ -56,34 +53,33 @@ module.exports.deleteReview = async (req, res, next) => {
   try {
     const { id } = req.params;
     const review = await reviewModel.findById(id);
-    if (!review) {
-      return next(new CustomError("Review not found", 404));
-    }
+    if (!review) return next(new CustomError("Review not found", 404));
     if (review.user.toString() !== req.user._id.toString()) {
       return next(
-        new CustomError("You are not authorized to delete this review", 401)
+        new CustomError("You are not authorized to update this review", 401)
       );
     }
-    await review.findByIdAndDelete();
+    await reviewModel.findByIdAndDelete(id);
     res.status(200).json({ message: "Review deleted successfully" });
   } catch (error) {
-    next(new CustomError("Could not delete review", 500));
+    next(new CustomError("Error deleting review", 500));
   }
 };
+
 module.exports.allReviews = async (req, res, next) => {
   try {
-    const propertyId = req.params;
+    const { propertyId } = req.params;
     const reviews = await reviewModel
       .find({
         property: propertyId,
       })
       .populate("user", "username email createdAt")
       .sort({ createdAt: -1 });
-    if (reviews.length == 0) {
-      return res.status(201).json([]);
+    if (reviews.length === 0) {
+      return res.status(200).json([]);
     }
-    res.status(200).json({ reviews });
+    res.status(200).json(reviews);
   } catch (error) {
-    next(new CustomError("Could not fetch reviews", 500));
+    next(new CustomError("Error fetching reviews", 500));
   }
 };
