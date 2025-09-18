9e1dbc7bed2e22acf3f22462081cebd133e34b73
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-09-18 12:57:40 +0530

    Final checks done! Everything works locally

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -17,7 +17,6 @@ module.exports.processPayment = async (req, res, next) => {
 
     const order = await RazorpayInstance.orders.create(options);
     res.status(200).json({ success: true, data: order });
-
   } catch (error) {
      console.error("Razorpay Error Object:", error);
      console.error("Razorpay Error Response Data:", error?.response?.data);
